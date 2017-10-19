5aeef9eb75915800daba6605ab6883b5c2d0c244
James Gardner <jgardner@mylifedigital.co.uk>
2017-10-19 16:37:43 +0100

    instrucciones

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+/**
+ * TODO:
+ * Bring in body parser (json) and use as middleware (app.use).
+ * Set up a basic route (app.get)
+ */
+
+const app = express();
+
+
+
+module.exports = app;
