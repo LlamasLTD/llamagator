68ea5743703399c260a9e105f72c24bb19c79e47
James Gardner <jgardner@mylifedigital.co.uk>
2018-05-23 12:18:15 +0100

    Added basic GET functionality

--- api/v1/github/pull.js
+++ api/v1/github/pull.js
@@ -1,10 +1,22 @@
+const db = require('../../../db');
+const { 
+    get
+} = require('../../../lib/pull')(db.model('PullRequest'));
+
+
 /**
  * Get a list of pull requests currently sat in the DB. 
  */
 module.exports = [
-    (req, res) => {
+    async (req, res) => {
+        const pulls = await get();
+
+        if (!pulls.length) {
+            return res.status('404').end();
+        }
+
         return res.json({
-            pulls: []
+            pulls
         });
     }
 ];
